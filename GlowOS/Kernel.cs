47ac06c743f348e2d5939927bc1096898ab9789d
PratyushKing <83279568+PratyushKing@users.noreply.github.com>
2024-03-11 00:44:09 +0530

    Add project files.

---
+++ GlowOS/Kernel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace GlowOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Welcome to GlowOS!");
+            
+        }
+
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            Console.Write("Text typed: ");
+            Console.WriteLine(input);
+        }
+    }
+}
