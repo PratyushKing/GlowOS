5fec49aa4d55cd0e60c31e1c616a5abc48ccc810
MrBisquit <william@wtdawson.info>
2024-03-10 20:22:54 +0000

    Added buggy code :)

--- GlowOS/Kernel.cs
+++ GlowOS/Kernel.cs
@@ -2,6 +2,7 @@
 using StardustOS.SDSystem;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Sys = Cosmos.System;
 
@@ -20,9 +21,11 @@ namespace GlowOS
             fs = new Sys.FileSystem.CosmosVFS();
             Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
 
+            if (!Directory.Exists("0:/GlowOS/")) Directory.CreateDirectory("0:/GlowOS/");
+
             BootConfig = ConfigMan.FetchConfig("0:/GlowOS/boot.cfg");
 
-            if(BootConfig.ContainsKey("STARTUPS"))
+            if (BootConfig.ContainsKey("STARTUPS"))
             {
                 int totalStartups = int.Parse(BootConfig["STARTUPS"]);
                 totalStartups += 1;
@@ -42,6 +45,13 @@ namespace GlowOS
             }
 
             ConfigMan.SaveConfig("0:/GlowOS/boot.cfg", BootConfig);
+
+            Console.WriteLine($"BootConfig elements count = {BootConfig.Count}");
+
+            foreach (var item in BootConfig)
+            {
+                Console.WriteLine($"{item.Key}={item.Value}");
+            }
         }
 
         protected override void Run()
